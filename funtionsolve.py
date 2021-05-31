22d873f9bc9f358d0a6612b25094f5ce6a6c652f
pondahai <dahai.pon@gmail.com>
2021-05-31 09:16:12 +0800

    Add files via upload

---
+++ funtionsolve.py
@@ -0,0 +1,10 @@
+from numpy import linspace,power,cos
+sum = 0
+for x in linspace(-2,2,1000):
+    sum = sum + (((power(x,3))*cos(x/2)+0.5)*power(4-power(x,2),0.5))*((4/1000))
+print (sum)
+
+
+from scipy import integrate
+f1=lambda x:(((x**3)*cos(x/2)+0.5)*((4-x**2)**0.5))
+print (integrate.quad(f1,-2,2))
